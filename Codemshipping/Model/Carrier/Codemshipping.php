f693c8e0cdfa269cef2d449ec4d4831d95f55f26
JinnahCodem <mohamedali.jinnah@codem.com>
2022-12-16 16:01:33 +0530

    Shipping Method Module

---
+++ Codemshipping/Model/Carrier/Codemshipping.php
@@ -0,0 +1,103 @@
+<?php declare(strict_types=1);
+/*
+ * Copyright (c) 2022 ReCodem Pvt Ltd All rights reserved
+ */
+
+namespace Codem\Codemshipping\Model\Carrier;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Psr\Log\LoggerInterface;
+use Magento\Quote\Model\Quote\Address\RateResult\Method;
+
+/**
+ * Class Codemshipping to display codem flat rate in checkout page
+ */
+class Codemshipping extends AbstractCarrier implements CarrierInterface
+{
+    /**
+     * @var string
+     */
+    protected $_code = 'codemshipping';
+
+    /**
+     * @var ResultFactory
+     */
+    protected $_rateResultFactory;
+
+    /**
+     * @var MethodFactory
+     */
+    protected $_rateMethodFactory;
+
+    /**
+     * Shipping constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ErrorFactory  $rateErrorFactory
+     * @param LoggerInterface $logger
+     * @param ResultFactory $rateResultFactory
+     * @param MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ErrorFactory $rateErrorFactory,
+        LoggerInterface $logger,
+        ResultFactory $rateResultFactory,
+        MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        $this->_rateResultFactory = $rateResultFactory;
+        $this->_rateMethodFactory = $rateMethodFactory;
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+    }
+
+    /**
+     * Get allowed methods
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+
+
+    /**
+     * @param RateRequest $request
+     * @return bool|Result
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        /** @var Result $result */
+        $result = $this->_rateResultFactory->create();
+
+        /** @var Method $method */
+        $method = $this->_rateMethodFactory->create();
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $amount = (float)$this->getConfigData('price');
+
+        $method->setPrice($amount);
+        $method->setCost($amount);
+
+        $result->append($method);
+
+        return $result;
+    }
+}
---
+++ Codemshipping/registration.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+/*
+ * Copyright (c) 2022 ReCodem Pvt Ltd All rights reserved
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Codem_Codemshipping', __DIR__);
